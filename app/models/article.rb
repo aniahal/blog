8f606bdcc01d40e2663f9ac68edd0773e2d1df3f
Ania <halkiewicz.anna@gmail.com>
2018-05-27 12:16:07 +0200

    Add sanitize tags method

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
+
+  def sanitize_tags(tags = '')
+    tags.downcase.split(',').map(&:strip).uniq
+  end
 end
