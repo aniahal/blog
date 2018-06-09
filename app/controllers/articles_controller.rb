355bdee2307b0cb0cdaca27d55fcc7ac14d92e85
Ania <halkiewicz.anna@gmail.com>
2018-06-09 10:08:57 +0200

    Restricts modification articles to authors only

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :update, :edit, :destroy]
-  
+  before_action :check_article_access, only: [:update, :edit, :destroy]
+
   http_basic_authenticate_with name: "admin", password: "secret",
     except: [:index, :show]
 
@@ -30,11 +31,11 @@ class ArticlesController < ApplicationController
   end
 
   def update
-      if @article.update(article_params)
-        redirect_to @article 
-      else 
-        render 'edit'
-      end
+    if @article.update(article_params)
+      redirect_to @article 
+    else 
+      render 'edit'
+    end
   end
 
   def edit
@@ -50,6 +51,11 @@ class ArticlesController < ApplicationController
 
   def find_article
     @article = Article.find(params[:id])
+  end
+
+  def check_article_access
+    return if @article.user == current_user
+    redirect_to articles_path
   end
 
   def article_params
