8afcec57f398b620546eec3446cae59c91d96873
Ania <halkiewicz.anna@gmail.com>
2018-06-09 12:16:29 +0200

    Adds only one like for article from one user option

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,7 @@
 class LikesController < ApplicationController
+  def create
+    article = Article.find(params[:article_id])
+    Like.create(user: current_user, article: article)
+    redirect_to article
+  end
 end
