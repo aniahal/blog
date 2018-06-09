85710de631b47e97f80c9fca4db8ba52d73c984f
Ania <halkiewicz.anna@gmail.com>
2018-06-09 12:58:33 +0200

    Adds unlike route and button

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,11 @@
 module ArticlesHelper
+  def already_liked?(user, article)
+    user_like(user, article).present?
+    # user.liked_articles.include? article
+    # article.likers.include? user
+  end 
+
+  def user_like(user, article)
+    Like.find_by(user: user, article: article)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'articles#index'
   resources :articles do
     resources :comments
-    resource :likes, only: [:create]
+    resources :likes, only: [:create, :destroy]
   end
 end
