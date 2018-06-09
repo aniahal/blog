d370858a0245f22a8e8398dee01540123ceded1c
Ania <halkiewicz.anna@gmail.com>
2018-06-09 11:46:25 +0200

    Adds Like model and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :articles
+  has_many :likes
+  has_many :liked_articles, through: :likes, source: :article
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'articles#index'
   resources :articles do
     resources :comments
+    resource :likes, only: [:create]
   end
 end
